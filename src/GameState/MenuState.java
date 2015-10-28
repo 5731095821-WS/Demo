bec6b4830c2c56629f1a4de82d5dbed206d711a4
5731095821-WS <wasuwach@hotmail.com>
2015-10-28 23:06:12 +0700

    finished Chapter1/7

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -1,6 +1,9 @@
+package GameState;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
 import com.sun.glass.ui.Size;
 
@@ -22,7 +25,7 @@ public class MenuState extends GameState {
 		super();
 		this.gsm=gsm;
 		try{
-			bg= new Background("/Background/menubg.gif", 1);
+			bg= new Background("/Backgrounds/menubg.gif", 1);
 			bg.setVector(-0.1, 0);
 			titleColor=new Color(128,0,0);
 			titleFont=new Font("Century Gothic",Font.PLAIN,28);
@@ -48,13 +51,58 @@ public class MenuState extends GameState {
 
 	@Override
 	public void draw(Graphics2D g) {
-		// TODO Auto-generated method stub
-
+		// draw bg
+		bg.draw(g);
+		
+		
+		//draw title
+		g.setColor(titleColor);
+		g.setFont(titleFont);
+		g.drawString("Dragon Tale",80,70);
+		
+		
+		//draw menu option
+		g.setFont(font);
+		for(int i=0;i<options.length;i++){
+			if(i==currentChoice){
+				g.setColor(Color.BLACK);
+				
+			}
+			else{
+				g.setColor(Color.RED);
+			}
+			g.drawString(options[i], 145, 140+i*15);
+		}
 	}
-
+private void select(){
+	if(currentChoice==0){
+		//start
+	}
+	if(currentChoice==1){
+		//help
+	}
+	if(currentChoice==2){
+		//quit
+		System.exit(0);
+	}
+}
 	@Override
 	public void KeyPressed(int k) {
-		// TODO Auto-generated method stub
+		if(k==KeyEvent.VK_ENTER){
+			select(); 
+		}
+		if(k==KeyEvent.VK_UP){
+			currentChoice--;
+			if(currentChoice==-1){
+				currentChoice=options.length-1;
+			}
+		}
+		if(k==KeyEvent.VK_DOWN){
+			currentChoice++;
+			if(currentChoice==options.length){
+				currentChoice=0;
+			}
+		}
 
 	}
 

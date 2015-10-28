3f6d0e23ecfbcdfbdbd87d4a73941750d27b30d5
5731095821-WS <wasuwach@hotmail.com>
2015-10-28 22:38:47 +0700

    asdasd

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -1,4 +1,8 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+
+import com.sun.glass.ui.Size;
 
 import GameState.GameState;
 import GameState.GameStateManager;
@@ -7,9 +11,27 @@ import TileMap.Background;
 
 public class MenuState extends GameState {
 	private  Background bg;
+	private int currentChoice=0;
+	private String[] options ={
+		"Start","Help","Quit"
+	};
+	private Color titleColor;
+	private Font titleFont;
+	private Font font;
 	public MenuState(GameStateManager gsm) {
 		super();
 		this.gsm=gsm;
+		try{
+			bg= new Background("/Background/menubg.gif", 1);
+			bg.setVector(-0.1, 0);
+			titleColor=new Color(128,0,0);
+			titleFont=new Font("Century Gothic",Font.PLAIN,28);
+			font=new Font("Arial",Font.PLAIN,12);
+			
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	@Override
